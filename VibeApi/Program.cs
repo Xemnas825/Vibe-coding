35ee5bcc208f26155a5c567c7b279e4f48bf6638
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-01-20 19:22:01 +0100

    actualizamos el program para usar los nuevos servicios

--- VibeApi/Program.cs
+++ VibeApi/Program.cs
@@ -1,4 +1,5 @@
 using VibeApi.Services;
+using VibeApi.Services.External;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,12 @@ builder.Services.AddScoped<IArticleService, ArticleService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ITagService, TagService>();
+builder.Services.AddScoped<IExternalPostService, ExternalPostService>();
+builder.Services.AddScoped<IExternalUserService, ExternalUserService>();
+builder.Services.AddScoped<IExternalAlbumService, ExternalAlbumService>();
+builder.Services.AddScoped<IExternalCommentService, ExternalCommentService>();
+builder.Services.AddScoped<IExternalPhotoService, ExternalPhotoService>();
+builder.Services.AddScoped<IExternalTodoService, ExternalTodoService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
